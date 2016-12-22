739f49e45d4ff07f1a01cb1fae14791ee4c10d7c
VladimirAnaniev <vladeto97@gmail.com>
2016-12-22 23:43:47 +0200

    Task 1 complete

--- Homework 2/Task1/fn81459_prob1_vc.cpp
+++ Homework 2/Task1/fn81459_prob1_vc.cpp
@@ -17,9 +17,108 @@
 
 using namespace std;
 
+double getSlope(double x1, double y1, double x2, double y2)
+{
+	if (x2 - x1 == 0) return 0;
+	return (y2 - y1) / (x2 - x1);
+}
+
+double getB(double slope, double x, double y)
+{
+	return y - slope*x;
+}
+
 void setNewPoint(double& xToChange, double& yToChange, double x, double y)
 {
-	//TODO: Kak da gi smenq basi?????
+	double slope = getSlope(xToChange, yToChange, x, y);
+	double b = getB(slope, xToChange, yToChange);
+
+
+	if (xToChange >= -5 && xToChange <= 5) // Above and below field
+	{
+		if (yToChange > 2)
+		{
+			yToChange = 2; 
+		}
+		else if(yToChange<-2)
+		{
+			yToChange = -2;  
+		}
+
+		if(slope!=0)
+			xToChange = (yToChange - b) / slope;
+	}
+
+	else if(yToChange>=-2 && yToChange<=2) //Right and left
+	{
+		if(xToChange<-5)
+		{
+			xToChange = -5; 
+		}
+		else if(xToChange>5)
+		{
+			xToChange = 5; 
+		}
+
+		yToChange = slope*xToChange + b;
+	}
+
+	else if(xToChange<-5 && yToChange>2) //Upper left
+	{
+		if(slope*-5+b>2)
+		{
+			yToChange = 2;
+			xToChange = (yToChange - b) / slope;
+		}
+		else
+		{
+			xToChange = -5;
+			yToChange = slope*xToChange + b;
+		}
+	}
+
+	else if (xToChange<-5 && yToChange<-2) //Lower Left
+	{
+		if (slope*-5 + b<-2)
+		{
+			yToChange = -2;
+			xToChange = (yToChange - b) / slope;
+		}
+		else
+		{
+			xToChange = -5;
+			yToChange = slope*xToChange + b;
+		}
+	}
+
+	else if (xToChange>5 && yToChange>2) //Upper right
+	{
+		if (slope*5 + b>2)
+		{
+			yToChange = 2;
+			xToChange = (yToChange - b) / slope;
+		}
+		else
+		{
+			xToChange = 5;
+			yToChange = slope*xToChange + b;
+		}
+	}
+
+	else if (xToChange>5 && yToChange<-2) //Lower right
+	{
+		if (slope*-5 + b<-2)
+		{
+			yToChange = -2;
+			xToChange = (yToChange - b) / slope;
+		}
+		else
+		{
+			xToChange = 5;
+			yToChange = slope*xToChange + b;
+		}
+	}
+
 }
 
 bool isOutside(double x, double y)
@@ -35,20 +134,20 @@ void distFormula(double &distance)
 double getDistance(double x1, double y1, double x2, double y2)
 {
 	double dist;
-	bool inside1 = isOutside(x1, y1);
-	bool inside2 = isOutside(x2, y2);
+	bool outside1 = isOutside(x1, y1);
+	bool outside2 = isOutside(x2, y2);
 
-	if (inside1 && inside2) //Both are outside
+	if (outside1 && outside2) //Both are outside
 	{
 		dist = 0;
 	}
 	else //At least one is inside
 	{
-		if(!inside1)
+		if(outside1)
 		{
 			setNewPoint(x1, y1, x2, y2);
 		}
-		else if(!inside2)
+		else if(outside2)
 		{
 			setNewPoint(x2, y2, x1, y1);
 		}
@@ -83,8 +182,6 @@ int main()
 	}
 
 	cout << fixed << setprecision(3) << distance;
-
-	system("pause");
 	
 	return 0;
 }
